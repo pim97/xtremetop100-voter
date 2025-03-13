8079aff327cb48de6b63f2f630a72b029530c4c9
Pim <36156855+pim97@users.noreply.github.com>
2025-03-13 15:13:29 +0100

    Update xtremetop100.js

--- xtremetop100.js
+++ xtremetop100.js
@@ -30,57 +30,34 @@ async function getUserInput(question) {
 }
 
 async function run() {
-    /**
-     * Creates a session with Scrappey
-     * This session uses a premium proxy located in the United States
-     */
-    const session = await scrappey.createSession({
-        // proxy: 'http://user:pass@ip:port' //Optional, if you want to use a specific proxy
-        "cloudflareBypass": true
-    })
 
-    const site = 1126084465;
-
-    const get = await scrappey.get({
-        url: `https://www.xtremetop100.com/in.php?site=${site}`,
-        session: session.session
-    });
-
-    const secureCaptcha = get.solution.response.match(
-        new RegExp(
-            `<input type="hidden" value="(\\d*)" name="secure_captcha_check"`
-        )
-    )[1];
-
-    const ticki = get.solution.response.match(
-        new RegExp(
-            `<input type="submit" value="(.*)" style="padding: 6px 10px 6px 10px;" name="ticki">`
-        )
-    )[1];
-
-    const image = await scrappey.get({
-        url: 'https://www.xtremetop100.com/securimage/securimage_show.php',
-        session: session.session,
-        base64: true
-    })
-
-    const base64Image = image.solution.base64Response;
-    const decodedImage = Buffer.from(base64Image, 'base64');
-
-    fs.writeFileSync('image.png', decodedImage);
-
-    const captchaCode = await getUserInput('Enter the captcha code: ');
-
-    const product = await scrappey.post({
-        url: "https://www.xtremetop100.com/in-post.php?site=1126084465",
-        postData: qs.stringify({
-            site: site,
-            secure_captcha_check: secureCaptcha,
-            phone_number: "",
-            captcha_code: captchaCode,
-            ticki: ticki
-        }),
-        session: session.session
+    const product = await scrappey.get{
+        "url": "https://www.xtremetop100.com/in.php?site=1132322794",
+        "browserActions": [
+            {
+                "type": "wait_for_selector",
+                "cssSelector": "[name='ticki']"
+            },
+            {
+                "type": "solve_captcha",
+                "captcha": "hcaptcha",
+                "captchaData": {
+                    "sitekey": "dcd2efd8-43d4-41bc-8e54-50682e8a8faa"
+                }
+            },
+            {
+                "type": "execute_js",
+                "code": "document.getElementsByName('g-recaptcha-response')[0].value = '{javascriptReturn[0]}'"
+            },
+            {
+                "type": "execute_js",
+                "code": "document.getElementsByName('h-captcha-response')[0].value = '{javascriptReturn[0]}'"
+            },
+            {
+                "type": "click",
+                "cssSelector": "[name='ticki']"
+            }
+        ]
     })
 
     // // Print the retrieved listing data
