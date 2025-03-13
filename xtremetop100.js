5721578a9fa7b31dd5f83f8be4bca43a908c6c9e
Pim <36156855+pim97@users.noreply.github.com>
2025-03-13 15:40:40 +0100

    Update xtremetop100.js

--- xtremetop100.js
+++ xtremetop100.js
@@ -10,25 +10,6 @@ const SCRAPPEY_API_KEY = 'API_KEY';
 // Create an instance of Scrappey
 const scrappey = new Scrappey(SCRAPPEY_API_KEY);
 
-/**
- * 
- * @param {*} question 
- * @returns 
- */
-async function getUserInput(question) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    return new Promise((resolve) => {
-        rl.question(question, (answer) => {
-            rl.close();
-            resolve(answer);
-        });
-    });
-}
-
 async function run() {
 
     const product = await scrappey.get{
