ab9bd3c72100201bb4a918416a242b5518d2e89c
Pim <36156855+pim97@users.noreply.github.com>
2025-04-09 14:54:16 +0200

    Update xtremetop100.js

--- xtremetop100.js
+++ xtremetop100.js
@@ -12,67 +12,10 @@ const scrappey = new Scrappey(SCRAPPEY_API_KEY);
 
 async function run() {
 
+    //Deprecated
     const product = await scrappey.get({
         "cmd": "request.get",
-        "url": "https://www.xtremetop100.com/in.php?site=1132375983",
-        "whitelistedDomains": ["recaptcha", "hcaptcha"],
-        "browserActions": [
-            {
-                "type": "solve_captcha",
-                "captcha": "turnstile"
-            },
-            {
-                "type": "wait_for_selector",
-                "cssSelector": "[name='ticki']"
-            },
-            {
-                "type": "if",
-                "condition": "[...document.querySelectorAll('script')].find(s => s.textContent.includes('var captchaChoice =')).innerText.includes('var captchaChoice = 1')",
-                "then": [
-                    {
-                        "type": "solve_captcha",
-                        "captcha": "hcaptcha",
-                        "captchaData": {
-                            "sitekey": "dcd2efd8-43d4-41bc-8e54-50682e8a8faa"
-                        }
-                    },
-                    {
-                        "type": "execute_js",
-                        "code": "document.getElementsByName('g-recaptcha-response')[0].value = '{javascriptReturn[0]}'"
-                    },
-                    {
-                        "type": "execute_js",
-                        "code": "document.getElementsByName('h-captcha-response')[0].value = '{javascriptReturn[0]}'"
-                    }
-                ],
-                "or": [
-                    {
-                        "type": "solve_captcha",
-                        "captcha": "recaptchav2",
-                        "captchaData": {
-                            "sitekey": "6Le7mwwbAAAAAKIGqjAy7l6mjqyvMQydDF2WI8-2",
-                            "fast": true
-                        }
-                    },
-                    {
-                        "type": "execute_js",
-                        "code": "document.getElementsByName('g-recaptcha-response')[0].value = '{javascriptReturn[0]}'"
-                    }
-                ]
-            },
-            {
-                "type": "click",
-                "cssSelector": "[name='ticki']"
-            },
-            {
-                "type": "execute_js",
-                "code": "document.querySelector(\"div[style*='display: grid;']\")?.setAttribute(\"style\", \"width: fit-content; margin: 0 auto;\");"
-            },
-            {
-                "type": "solve_captcha",
-                "captcha": "turnstile"
-            }
-        ]
+        "url": "https://www.xtremetop100.com/in.php?site=1132375983"
     })
     // // Print the retrieved listing data
     console.log(JSON.stringify(product, null, 2));
